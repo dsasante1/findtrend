186ebbb103b46f717157a19b61879a77f85b65a5
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-08-17 18:25:36 +0000

    findtrend in vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <TheHeader />
   </header>
 </template>
 
