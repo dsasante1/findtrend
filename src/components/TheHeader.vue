2b01d6c25ac0bd01963ae2ebf5232026db43fe2c
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-08-18 12:54:08 +0000

    done social media cards

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -22,6 +22,38 @@
             <span id="getStarted">
                 <img alt="get-started" id="getStartedImage" src="@/assets/Get Started 🔥.svg"/>
             </span>
+            <div id="socialMedia">
+        <span id="cardTwitter">
+             <img alt="cardTwitter"  src="@/assets/image 96.png"/>
+             <span id="socialMediaText">Cryptopunk - Search</span>
+             <img id="closeIcon"  src="@/assets/Group 1.svg"/>
+        </span>
+
+        <span id="cardPininterest">
+             <img alt="cardPininterest"  src="@/assets/image 96(1).png"/>
+             <span id="socialMediaText">Popular Design - Search</span>
+             <img id="closeIcon"  src="@/assets/Group 1.svg"/>
+        </span>
+
+
+        <span id="cardFb">
+             <img alt="cardFb"  src="@/assets/image 95.png"/>
+             <span id="socialMediaText">Product Design - Search</span>
+             <img id="closeIcon"  src="@/assets/Group 1.svg"/>
+        </span>
+        
+
+
+
+
+        <span id="cardDribble">
+             <img alt="cardDribble" id="cardDribbleImage"  src="@/assets/Rectangle 6.svg"/>
+             <span id="socialMediaText">Elon Musk - Search</span>
+             <img id="closeIcon"  src="@/assets/Group 1.svg"/>
+        </span>
+
+
+    </div>
         </div>
     </div>
 
@@ -149,10 +181,94 @@ Findtrend  let you gathers all of your favorite website into one place.
     border-radius: 50px;
     position: relative;
     left: 100px;
-
- 
-
 }
+
+
+
+#socialMedia{
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    
+}
+
+
+
+
+#socialMediaText{
+    color: white;
+    font-size: 15px;
+}
+
+
+#closeIcon{
+    width: 20px;
+}
+
+#cardTwitter{
+    background-color: #181818;
+    padding-left: 20px;
+    padding-right: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    width: 300px;
+    display: flex;
+    gap: 20px;
+    rotate: -5deg;
+    /* padding: 20px; */
+}
+
+
+#cardFb{
+    background-color: #181818;
+    padding-left: 20px;
+    padding-right: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    width: 300px;
+    display: flex;
+    gap: 20px;
+    rotate: -5deg;
+    /* padding: 20px; */
+}
+
+
+#cardPininterest{
+    background-color: #181818;
+    padding-left: 20px;
+    padding-right: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    width: 302px;
+    display: flex;
+    gap: 20px;
+    rotate: 15deg;
+    /* padding: 20px; */
+}
+
+
+#cardDribbleImage{
+    width: 30px;
+    
+}
+
+#cardDribble{
+    background-color: #181818;
+    padding-left: 20px;
+    padding-right: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    width: 300px;
+    display: flex;
+    gap: 20px;
+    rotate: 15deg;
+    /* padding: 20px; */
+}
+
+
+
+
 
 
 
