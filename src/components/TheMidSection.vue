92ed03312bf6a3c85430942555925e08f4eafdbf
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-08-18 15:10:37 +0000

    work on mid section

--- src/components/TheMidSection.vue
+++ src/components/TheMidSection.vue
@@ -2,6 +2,101 @@
 <script setup></script>
 <template>
 
+
+<div id="mobileView">
+    <div class="midSection">
+        <p id="newTab">Open new tabs is sh*t </p>
+        
+        
+        <img alt="you just need one tab" id="oneTab" src="@/assets/You just need one tab now.svg"/>
+        <img alt="arrow" id="topArrow" src="@/assets/Arrow 41.svg"/>
+
+
+        <div id="centerContent">
+
+                <span id="whiteBackgroud">
+                    <span id="blackBackground">
+                        <img alt="midLogo"  src="@/assets/green_logo.svg"/>
+                        <span>Findtrend - Elon Musk</span>
+                        <img alt="closeIcon"  src="@/assets/Group 1.svg"/>
+                    </span>
+
+                </span>
+            
+        </div>
+
+        
+        
+        <img alt="arrow" id="topArrow" src="@/assets/Arrow 61.svg"/>
+        <img alt="discover trend" id="discoverTrends" src="@/assets/solution for discover a trend.svg"/>       
+        <p id="subText">
+        A solution for your browser tabs and don’t make your device get slower over time. Get ease and faster to discover a trend with just one tab.
+        </p>
+            
+       
+
+    </div>
+    
+    <div class="companies">
+        
+        <p id="promotionText">
+            Findtrend helps you to increase your productivity and reduce your
+             computer's memory load, an application that can fulfill your daily
+              browsing needs.
+        </p>
+        <p id="promotionText">
+            Findtrend make +1000 Startup grow
+        </p>
+
+        <div id="companyGrid">
+            <img alt="microsoft" src="@/assets/image 116.svg"/>
+            <img alt="allianz"  src="@/assets/image 128.png"/>  
+            <img alt="work"  src="@/assets/image 121.svg"/>
+            <img alt="volvo"  src="@/assets/image 119.svg"/>
+            <img alt="grab" src="@/assets/image 125.png"/>
+            <img alt="verizon" src="@/assets/image 122.svg"/>
+            <img alt="coinbase" src="@/assets/image 133.png"/>
+            <img alt="expedia" src="@/assets/image 129.png"/>
+            <img alt="airtable" src="@/assets/image 117.png"/>
+            <img alt="magento" src="@/assets/image 126.svg"/>
+            <img alt="carrara" src="@/assets/image 120.png"/>
+            <img alt="bonanza" src="@/assets/image 124.png"/>
+            <img alt="transfer" src="@/assets/image 132.png"/>
+            <img alt="suzuki" src="@/assets/image 127.png"/>
+            <img alt="algolia" src="@/assets/image 118.png"/>
+            <img alt="heineken" src="@/assets/image 130.png"/>
+            
+            
+        </div>
+        <p id="promotionText">
+            All platform connect to Findtrend
+        </p>
+        <div id="socialMediaLinks">
+            <img alt="fb" src="@/assets/image 105.svg"/>
+            <img alt="twitter"  src="@/assets/image 96.svg"/>  
+            <img alt="work"  src="@/assets/Rectangle 6.svg"/>
+            <img alt="volvo"  src="@/assets/image 111.svg"/>
+            <img alt="grab" src="@/assets/image 113.svg"/>
+            <img alt="verizon" src="@/assets/image 114.svg"/>
+            <img alt="coinbase" src="@/assets/image 115.svg"/>
+            <img alt="expedia" src="@/assets/image 112.svg"/>
+           
+        </div>
+        <div id="twitterImages">
+            <img alt="twitter1" src="@/assets/tweet 1.svg"/>
+            <img alt="twitter2" src="@/assets/tweet 2.svg"/>
+            <img alt="twitter3" src="@/assets/tweet 3.svg"/>
+            
+        </div>
+        <div id="moreTrendButton">
+            <span id="moreTrends">View More Trend</span>
+
+        </div>
+        
+    </div>
+
+</div>
+
 <div id="desktopView">
     <div class="midSection">
         <p id="newTab">Open new tabs is sh*t </p>
@@ -93,7 +188,104 @@
 <style scoped>
 
 
-@media only screen and (max-width: 900px){}
+@media only screen and (max-width: 900px){
+    .midSection{
+    background-color: greenyellow;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 30px;
+
+}
+
+#newTab{
+    font-size: 30px;
+    font-weight: bolder;
+    padding-top: 30px;
+    text-align: center;
+
+}
+
+#centerContent{
+    display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#discoverTrends{
+    width: 130px;
+    position: relative;
+    left: 190px;
+    top: 80px;
+}
+
+
+#topArrow{
+    width: 70px;
+    position: relative;
+    left: 200px;
+    top: 60px;
+}
+
+#downArrow{
+    width: 50px;
+    position: relative;
+    left:280px;
+    top: 90px;
+}
+
+#oneTab{
+    width: 130px;
+    position: relative;
+    left: 70px;
+}
+
+
+#whiteBackgroud{
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    background-color: white;
+    border-radius: 5px;
+    
+   
+}
+
+#blackBackground{
+    background-color: black;
+    color: white;
+    padding-left: 80px;
+    padding-right: 80px;
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+
+
+
+}
+
+/* #blackBackground span {
+
+} */
+
+#subText{
+    font-size: large;
+    padding: 50px;
+    padding-left: 300px;
+    padding-right: 300px;
+    text-align: center;
+    
+}
+
+
+.companies{
+    background-color: white;
+    padding: 50px;
+}
+
+}
 
 
 @media only screen and (min-width: 900px){
