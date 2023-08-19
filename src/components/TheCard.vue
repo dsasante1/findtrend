05ded8fa9ef0e17213331b2e4fbfd9acae52aea2
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-08-19 11:44:17 +0000

    website done. start defined props

---
+++ src/components/TheCard.vue
@@ -0,0 +1,53 @@
+
+<script setup></script>
+
+
+<template>
+
+<span id="cardComponent">
+    <h1>Personal</h1>
+    <h3>Special first packet for all</h3>
+    <br>
+    <hr>
+    <br>
+    <span id="cost">$8</span><span>/Month</span>
+    <div>
+        <div>
+<img alt="checkMark" id="checkMark" src="@/assets/Frame 29.svg"/>
+<span>Up to 5 page each group</span>
+        </div>
+
+        <div>
+<img alt="checkMark" id="checkMark" src="@/assets/Frame 29.svg"/>
+<span>Up to 10 group page</span>
+        </div>
+
+        <div>
+<img alt="checkMark" id="checkMark" src="@/assets/Frame 29.svg"/>
+<span>5 Days group page saved</span>
+        </div>
+
+    </div>
+
+    <div id="freeTrialPositionOne">
+        <span id="freeTrial">Start Free Trial</span>
+    </div>
+
+</span>
+
+</template>
+
+
+<style scoped>
+
+#cardComponent{
+    width: 320px;
+    height: 650px;
+    background-color: white ;
+    border-radius: 5px;
+    padding: 20px;
+
+}
+
+
+</style>
